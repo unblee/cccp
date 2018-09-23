d44d0658957aa9b73b42b8f3e05e7ebd3848dc99
unblee <unblee@users.noreply.github.com>
2018-09-23 21:18:09 +0900

    add HTTPRequest source

--- source/url.go
+++ source/url.go
@@ -1,48 +1,15 @@
 package source
 
 import (
-	"fmt"
-	"io"
 	"net/http"
-	"strconv"
 
 	"github.com/pkg/errors"
 )
 
-type URL struct {
-	reader io.ReadCloser
-	size   uint64
-}
-
-func NewURL(rawurl string) (*URL, error) {
-	resp, err := http.Get(rawurl)
+func NewURL(rawurl string) (*HTTPRequest, error) {
+	req, err := http.NewRequest(http.MethodGet, rawurl, nil)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to http get '%s'", rawurl)
+		return nil, errors.Wrap(err, "failed to create new http request")
 	}
-
-	size, err := strconv.ParseUint(resp.Header.Get("Content-Length"), 10, 64)
-	if err != nil {
-		return nil, errors.Wrapf(err, "there is no content on '%s'", rawurl)
-	}
-
-	if resp.StatusCode >= 400 {
-		return nil, fmt.Errorf("failed to get a file '%s': %d %s", rawurl, resp.StatusCode, http.StatusText(resp.StatusCode))
-	}
-
-	return &URL{
-		reader: resp.Body,
-		size:   size,
-	}, nil
-}
-
-func (u *URL) Read(p []byte) (n int, err error) {
-	return u.reader.Read(p)
-}
-
-func (u *URL) Close() error {
-	return u.reader.Close()
-}
-
-func (u *URL) Size() uint64 {
-	return u.size
+	return NewHTTPRequest(req)
 }
